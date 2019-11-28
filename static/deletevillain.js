89a942be98334bbfb2ddeecd6ef05613240ee9e1
Ann Cascarano <a.cascarano@gmail.com>
2019-11-28 12:34:29 -0500

    Delete functionality improvements
    Select element for choosing from existing villain names

--- static/deletevillain.js
+++ static/deletevillain.js
@@ -1,18 +1,30 @@
 $(document).ready(() => {
-  document.getElementById('form').onsubmit = handleDeleteForm;
+	document.getElementById("form").onsubmit = handleDeleteForm;
 });
 
-function handleDeleteForm(event){
-  event.preventDefault();
-  var name = $("input[name='name']").val();
-  $.post("/api/villains/delete", {
-    "name":name
-  }, function(data){
-    if (data.errors !== undefined) {
-      document.getElementById("errors").innerHTML = data.errors.map((error) => (`<div class="error">${error}</div>`)).join("");
-    } else {
-      window.location = "/";
-    }
-  });
-  return false;
+$.get("/api/villains/", function(data) {
+	for (let i in data) {
+		$("#villain-name").append(`<option>${data[i].name}</option>`);
+	}
+});
+
+function handleDeleteForm(event) {
+	event.preventDefault();
+	var name = $("select[name='name']").val();
+	$.post(
+		"/api/villains/delete",
+		{
+			name: name
+		},
+		function(data) {
+			if (data.errors !== undefined) {
+				document.getElementById("errors").innerHTML = data.errors
+					.map(error => `<div class="error">${error}</div>`)
+					.join("");
+			} else {
+				window.location = "/";
+			}
+		}
+	);
+	return false;
 }
